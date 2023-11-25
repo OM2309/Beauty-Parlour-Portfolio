7cccb9ec91266dcbbf93e996b54c7219f0ac069e
anurag <23anuragsharma@gmail.com>
2023-11-25 18:42:53 +0530

    Home

--- index.php
+++ index.php
@@ -7,26 +7,37 @@
     <title>Beauty Parlour</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="shortcut icon" href="./images/favicon.png" type="image/x-icon">
 </head>
 
 <body>
 
     <?php include './components/Navbar.php' ?>'
 
- 
+    <section class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="hero__img">
+                    <img src="./images/home-img.png" class="object-bottom" alt="">
+                </div>
+                <div class="hero__content">
+                    <h1> Your Big Day<br><span>in Style</span></h1>
+                    <p>Come and see our bride's corner and take the opportunity to schedule your date.</p>
+                    <a href="#" class="hidden md:block text-white bg-[#75492b] hover:bg-[#75492b] focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center">Contact</a>
+                </div>
+            </div>
+        </div>
+    </section>
 
-    ?>
-    <div class="-mt-6">
-        <img class="w-full object-cover h-[400px] md:h-[600px]" src="./images/5.jpg" alt="image description">
+
+    <div class="mt-2 py-4">
+        <h1 class="font-style text-3xl text-center mb-4 font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl ">Our<span class="text-[#75492b]"> Services</span></h1>
     </div>
 
-    <div class="mt-4 py-4 ">
-        <h1 class="text-center mb-4 text-4xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl ">Our<span class="underline decoration-wavy underline-offset-3 decoration-8 decoration-blue-400 "> Services</span></h1>
-    </div>
 
-
-    <div class="card flex justify-around items-center flex-wrap mx-4 my-6  md:m-6">
-        <div class=" mb-4  max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
+    <div class="card flex justify-around items-center flex-wrap  my-4  p-8 w-full">
+        <div class="mb-4 max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
             <a href="#">
                 <img class="rounded-t-lg" src="./images/1.jpg" />
             </a>
@@ -35,7 +46,7 @@
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
                 </a>
                 <p class="mb-3 font-normal text-gray-700 ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#75492b] rounded-lg  ">
                     Read more
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
@@ -52,7 +63,7 @@
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
                 </a>
                 <p class="mb-3 font-normal text-gray-700 ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#75492b] rounded-lg ">
                     Read more
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
@@ -69,7 +80,7 @@
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>
                 </a>
                 <p class="mb-3 font-normal text-gray-700 ">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#75492b] rounded-lg ">
                     Read more
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
@@ -80,22 +91,40 @@
     </div>
 
 
-    <div class="mt-4">
-        <h1 class="text-center mb-4 text-4xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl ">Our<span class="underline decoration-wavy underline-offset-3 decoration-8 decoration-blue-400 "> Testimonials</span></h1>
+    <div class="mt-4 p-6">
+        <h1 class="text-3xl font-style text-center mb-4 font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl ">Our<span class="text-[#75492b] "> Testimonials</span></h1>
     </div>
 
- <section class="text-gray-600 body-font">
+    <section class="text-gray-600 body-font services">
         <div class="container px-5 py-6 mx-auto">
-           <div class="flex flex-wrap">
-               
-           <div class="p-4 md:w-1/2 w-full">
+            <div class="flex flex-wrap">
+
+                <div class="p-4 md:w-1/2 w-full">
                     <div class="h-full bg-white p-8 rounded border border-gray-200 rounded-lg shadow ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="#75492b" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
                             <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                        </svg>
                         </svg>
                         <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
                         <a class="inline-flex items-center">
-                            <img alt="testimonial" src="https://dummyimage.com/106x106" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
+                            <img alt="testimonial" src="./images/6.jpg" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
+                            <span class="flex-grow flex flex-col pl-4">
+                                <span class="title-font font-medium text-gray-900">Holden Caulfield</span>
+                                <span class="text-gray-500 text-sm">UI DEVELOPER</span>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+             
+                <div class="p-4 md:w-1/2 w-full">
+                    <div class="h-full bg-white p-8 rounded border border-gray-200 rounded-lg shadow ">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="#75492b" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+                            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                        </svg>
+                        </svg>
+                        <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
+                        <a class="inline-flex items-center">
+                            <img alt="testimonial" src="./images/6.jpg" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
                             <span class="flex-grow flex flex-col pl-4">
                                 <span class="title-font font-medium text-gray-900">Holden Caulfield</span>
                                 <span class="text-gray-500 text-sm">UI DEVELOPER</span>
@@ -104,25 +133,55 @@
                     </div>
                 </div>
 
-                <div class="p-4 md:w-1/2 w-full">
-                    <div class="h-fullbg-white p-8 rounded border border-gray-200 rounded-lg shadow">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
-                            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-                        </svg>
-                        <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
-                        <a class="inline-flex items-center">
-                            <img alt="testimonial" src="https://dummyimage.com/107x107" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
-                            <span class="flex-grow flex flex-col pl-4">
-                                <span class="title-font font-medium text-gray-900">Alper Kamu</span>
-                                <span class="text-gray-500 text-sm">DESIGNER</span>
-                            </span>
-                        </a>
-                    </div>
-                </div>
+               
             </div>
         </div>
     </section>
 
+    <div class="mt-4 p-6">
+        <h1 class="text-3xl font-style text-center mb-4 font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl ">Our<span class="text-[#75492b] "> Gallery</span></h1>
+    </div>
+
+
+    <section class="gallery section -mt-32" id="gallery">
+            <div class="gallery__content">
+                <!-- <div class="row">
+                    <div class="section__title ">
+                        <h1 class='text-[#75492b] font-bold'>Gallery</h1>
+                      
+                    </div>
+                </div> -->
+                <div class="gallery__list__img row container p-6">
+                    <div class="gallery__img">
+                        <img src="./images/foto1.png" alt="">
+                    </div>
+                    <div class="gallery__img">
+                        <img src="./images/foto2.png" alt="">
+                    </div>
+                    <div class="gallery__img">
+                        <img src="./images/foto3.png" alt="">
+                    </div>
+                    <div class="gallery__img">
+                        <img src="./images/foto4.png" alt="">
+                    </div>
+                    <div class="gallery__img">
+                        <img src="./images/foto5.png" alt="">
+                    </div>
+                    <!-- <div class="gallery__img">
+                        <img src="./images/foto6.png" alt="">
+                    </div>
+                    <div class="gallery__img">
+                        <img src="./images/foto7.png" alt="">
+                    </div>
+                    <div class="gallery__img">
+                        <img src="./images/foto8.png" alt="">
+                    </div> -->
+                </div>
+                <!-- <button class="btn">View more</button> -->
+            </div>
+        </section>
+
+        <?php include './components/Footer.php' ?>'
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
